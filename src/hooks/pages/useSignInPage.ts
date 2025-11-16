7b80bd1e4027c0e00041a7c1f7a815b4c4c0e303
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2025-11-16 22:06:22 +0100

    fix admin creation slice

--- src/hooks/pages/useSignInPage.ts
+++ src/hooks/pages/useSignInPage.ts
@@ -1,12 +1,15 @@
-import {useEffect, useState} from 'react'
-import { useNavigate } from '@tanstack/react-router'
+import { useEffect, useState } from 'react'
+import { useNavigate, useSearch } from '@tanstack/react-router'
 import { authServiceApi } from '../../api/auth.api'
 import { ROUTES } from '../../util/constants.util.ts'
 import type { FormEvent } from 'react'
 import type { AuthAPIResponse } from '../../types/response.payload.types'
 
 export const useSignInPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const { email: queryEmail, password: queryPassword } = useSearch({ from: '/' })
 
   const [showPassword, setShowPassword] = useState(false)
   const [email, setEmail] = useState('')
@@ -18,6 +21,11 @@ export const useSignInPage = () => {
   useEffect(() => {
     pingAdminUser();
   }, []);
+  
+  useEffect(() => {
+    setEmail(queryEmail);
+    setPassword(queryPassword);
+  }, [queryEmail, queryPassword]);
   
   const pingAdminUser = async () => {
     const { success } = await authServiceApi.pingAdmin();
